d44ff6d7f9c728bda3229861d4805578ed957392
Yukti <yuktimainali2@gmail.com>
2023-06-13 22:54:33 +0530

    intro-ui-development

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-function setActive(event) {
-    var links = document.querySelectorAll(".navigation-links li a");
-    links.forEach(function(link) {
-        link.classList.remove("active");
+// JavaScript
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    // Remove "active" class from all links
+    navLinks.forEach((link) => {
+      link.classList.remove('active');
     });
-    event.target.classList.add("active");
-    event.preventDefault(); // Prevent default link behavior
-}
+
+    // Add "active" class to the clicked link
+    link.classList.add('active');
+  });
+});
