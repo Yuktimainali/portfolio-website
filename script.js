d6a38a55a0ca65e6a01fdb96eb1ba510488ae228
Yukti <yuktimainali2@gmail.com>
2023-06-19 23:27:47 +0530

    intro-ui

--- script.js
+++ script.js
@@ -1,17 +1,8 @@
-const navLinks = document.querySelectorAll('.navlink');
-
-navLinks.forEach((link) => {
-  link.addEventListener('click', () => {
-    if (!link.classList.contains('active')) {
-      // Remove "active" class from all links
-      navLinks.forEach((link) => {
-        link.classList.remove('active');
-        link.style.color = ''; // Reset the color
-      });
-
-      // Add "active" class to the clicked link
-      link.classList.add('active');
-      link.style.color = 'rgba(199, 3, 3, 0.842)';
-    }
-  });
-});
+var counter = 1;
+    setInterval(function(){
+      document.getElementById('radio' + counter).checked = true;
+      counter++;
+      if(counter > 4){
+        counter = 1;
+      }
+    }, 2500);
