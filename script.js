6c1163846c0b0d35e4fc7657f573f1e32a722a26
VE00YM607@globalymc.com <VE00YM607@globalymc.com>
2023-06-22 12:21:32 +0530

    intro-ui

--- script.js
+++ script.js
@@ -6,3 +6,19 @@ var counter = 1;
         counter = 1;
       }
     }, 2500);
+
+    const textElement = document.getElementById("text-animation");
+    const initialText = textElement.textContent;
+    
+    setInterval(() => {
+      textElement.style.animation = "none";
+      textElement.textContent = textElement.textContent.includes("Software") ? "I am also a Web Developer." : "I am a Software Developer.";
+      void textElement.offsetWidth; // Trigger reflow
+      textElement.style.animation = "text-appear 0.7s ease-in-out";
+    }, 3500);
+    
+
+
+    
+
+    
