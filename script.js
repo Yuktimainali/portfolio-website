64429a01a0a53f0c89ed584b033d7d028bc5b3aa
Yukti <yuktimainali2@gmail.com>
2023-06-25 00:17:44 +0530

    intro-ui-complete

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ const initialText = textElement.textContent;
 
 setInterval(() => {
   textElement.style.animation = "none";
-  textElement.textContent = textElement.textContent.includes("Software") ? "I am a Web Developer." : "I am a Software Developer.";
+  textElement.textContent = textElement.textContent.includes("Software") ? "I am also a Web Developer." : "I am a Software Developer.";
   void textElement.offsetWidth; // Trigger reflow
   textElement.style.animation = "text-appear 0.7s ease-in-out";
 }, 3500);
