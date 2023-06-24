964bec08764d77eef02f6dd15bd658cbce912fd7
Yukti <yuktimainali2@gmail.com>
2023-06-24 19:02:34 +0530

    intro-ui

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const navLinks = document.querySelectorAll('.nav-bar li a');
 
 navLinks.forEach(link => {
-  link.addEventListener('click', function() {
+  link.addEventListener('click', function () {
     // Remove 'active' class from all nav links
     navLinks.forEach(navLink => {
       navLink.classList.remove('active');
@@ -14,30 +14,29 @@ navLinks.forEach(link => {
 
 
 var counter = 1;
-    setInterval(function(){
-      document.getElementById('radio' + counter).checked = true;
-      counter++;
-      if(counter > 4){
-        counter = 1;
-      }
-    }, 2500);
+setInterval(function () {
+  document.getElementById('radio' + counter).checked = true;
+  counter++;
+  if (counter > 4) {
+    counter = 1;
+  }
+}, 2500);
 
-    const textElement = document.getElementById("text-animation");
-    const initialText = textElement.textContent;
-    
-    setInterval(() => {
-      textElement.style.animation = "none";
-      textElement.textContent = textElement.textContent.includes("Software") ? "I am also a Web Developer." : "I am a Software Developer.";
-      void textElement.offsetWidth; // Trigger reflow
-      textElement.style.animation = "text-appear 0.7s ease-in-out";
-    }, 3500);
+const textElement = document.getElementById("text-animation");
+const initialText = textElement.textContent;
 
-    
-
- 
+setInterval(() => {
+  textElement.style.animation = "none";
+  textElement.textContent = textElement.textContent.includes("Software") ? "I am a Web Developer." : "I am a Software Developer.";
+  void textElement.offsetWidth; // Trigger reflow
+  textElement.style.animation = "text-appear 0.7s ease-in-out";
+}, 3500);
 
 
 
-    
 
-    
+
+
+
+
+
