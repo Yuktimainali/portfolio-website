051c6903c530b088eea99a21a522c334e18ac459
Yukti <yuktimainali2@gmail.com>
2023-06-25 22:36:14 +0530

    skills-ui

--- script.js
+++ script.js
@@ -57,3 +57,32 @@ setInterval(() => {
   textElement.style.animation = "text-appear 0.7s ease-in-out";
 }, 3500);
 
+var swiper = new Swiper(".slide-content", {
+  slidesPerView: 3,
+  spaceBetween: 25,
+  loop: true,
+  centerSlide: 'true',
+  fade: 'true',
+  grabCursor: 'true',
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints:{
+      0: {
+          slidesPerView: 1,
+      },
+      520: {
+          slidesPerView: 2,
+      },
+      950: {
+          slidesPerView: 3,
+      },
+  },
+});
